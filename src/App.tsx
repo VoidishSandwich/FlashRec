9f1dfcd1385679f1ab826f8c338bc181be014e7a
GavinTalks <gavinthebigbrain@gamil.com>
2026-06-07 19:44:58 +0800

    Uh real fix for redirection I hope

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom';
-import {MainLayout} from './pages/MainLayout'; // 🚀 引入剛寫好的外殼
-import {Home} from './pages/Home';
-import {Settings} from './pages/Settings'; 
-import {Error404} from './pages/404';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+import { MainLayout } from './pages/MainLayout'; // 🚀 引入剛寫好的外殼
+import { Home } from './pages/Home';
+import { Settings } from './pages/Settings';
+import { Error404 } from './pages/404';
 
 const router = createBrowserRouter([{
         path: '/',
-        element: <MainLayout />, // 🚀 外殼套在這裡
+        element: <MainLayout />,
         children: [{
-            path: '', // 當網址是 / 時，自動導向 /home
+            path: '',
             element: <Navigate to="/home" replace />,
         }, {
-            path: 'home', // 網址會自動組合成 /home
+            path: 'home',
             element: <Home />,
         }, {
-            path: 'settings', // 網址會自動組合成 /settings
+            path: 'settings',
             element: <Settings />,
         }, {
             path: "*",
@@ -22,9 +22,12 @@ const router = createBrowserRouter([{
         }],
     }, {
         path: '*',
-        element: <Navigate to="/home" replace />,
+        element: <Navigate to="home" replace />,
     },
-]);
+], {
+  basename: '/FlashRec'
+});
+
 
 export default function App() {
     return <RouterProvider router={router} />;
